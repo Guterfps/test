5f2f6fd89ab75c429fcc058c197665211e523fc6
guter <ithayg@gmail.com>
2019-07-04 11:04:36 +0300

    request

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const request = require('request')
+const port = process.env.SERVER_PORT || 8080
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+
+app.get("/sanity",function(req,res){
+   res.send(console.log("OK"))
+})
+
+
+app.get("/recipes/:ingredient",function(req,res){
+    let Ingredient=req.params.ingredient
+    request.get(`https://recipes-goodness.herokuapp.com/recipes/${Ingredient}`,function(error, response, body){
+        console.log(body)
+    })
+})
+
+
+
+
+
+
+app.listen( port, () => console.log( `Running server on port ${ port }` ) )
